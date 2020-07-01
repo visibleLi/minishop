78b4c07bad2dc20d5d0704d1208651b2a221a11d
lmany <864494871@qq.com>
2020-07-01 18:29:22 +0800

    基础页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../components/Home.vue";
-import Admin from "../pages/Admin.vue";
+//import Admin from "../pages/Admin.vue";
 import Cart from "../pages/Cart.vue";
+
+import Index from "../pages/admin/Index";
+import New from "../pages/admin/New";
+import Edit from "../pages/admin/Edit";
+import Products from "../pages/admin/Products";
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -14,7 +20,24 @@ const routes = [
   {
     path: "/admin",
     name: "Admin",
-    component: Admin
+    component: Index,
+    children: [
+      {
+        path: "new",
+        name: "New",
+        component: New
+      },
+      {
+        path: "edit/:id",
+        name: "Edit",
+        component: Edit
+      },
+      {
+        path: "",
+        name: "Products",
+        component: Products
+      }
+    ]
   },
   {
     path: "/cart",
